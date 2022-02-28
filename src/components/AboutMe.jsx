59f3254708e6eb4a358b0f3415b1196547aa9ecd
gonzaleztucci <gonzaleztucci@gmail.com>
2022-02-28 19:56:05 +0100

    ready to merge structure

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+  padding: 3rem;
+`
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <Container>
+      
+    </Container>
   )
 }
 
